1520ca59ce52ed0784f429c3c7d357299ffe9d93
christian <christian.ambriz@gmail.com>
2016-10-24 13:38:08 -0700

    add user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_authentication_token!
+  
+  validates :auth_token, uniqueness: true
+
   has_many :daily_categories
   has_many :dailies, through: :daily_categories
+
+  def generate_authentication_token!
+    begin 
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
