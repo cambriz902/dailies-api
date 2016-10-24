36a3f468555dc33ceb183896224b4f66dd19f4bc
christian <christian.ambriz@gmail.com>
2016-10-23 18:03:08 -0700

    user test pass validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@
 
 require 'rails_helper'
 
-describe User do
+RSpec.describe User, type: :model do
   before { @user = FactoryGirl.build(:user) }
 
   subject { @user }
@@ -31,7 +31,7 @@ describe User do
   it { should be_valid }
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
-  it { should vallidate_confirmation_of(:password) }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) } 
 end
