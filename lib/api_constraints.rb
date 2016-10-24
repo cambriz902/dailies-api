ff3b3ed0cad815410dbbc690ee9b722739012d34
christian <christian.ambriz@gmail.com>
2016-10-23 23:10:51 -0700

    remove subdomain since it doesnt work with heroku

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,6 @@ class ApiConstraints
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.dailies.v#{@version}")
+    @default || req.headers['Accept'].include?("application/dailies.v#{@version}")
   end
 end
