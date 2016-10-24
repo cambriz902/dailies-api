1a010e1aeb288197a3449dfb7bb2424fae3262d0
christian <christian.ambriz@gmail.com>
2016-10-23 22:53:15 -0700

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
             constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      devise_for :users
+      resources :users, only: [:show]
 
       resources :dailies, only: [:show, :create] do
         collection do 
