e57d7809fb7847d8c867186511a0f7464a4a9647
christian <christian.ambriz@gmail.com>
2016-10-25 18:54:56 -0700

    removed code to build using TDD and adde controller spec file'

--- app/controllers/api/v1/daily_categories_controller.rb
+++ app/controllers/api/v1/daily_categories_controller.rb
@@ -1,29 +1,4 @@
 class Api::V1::DailyCategoriesController < ApplicationController
-  respond_to :json
-  before_action :authenticate_user!
-
-  def show
-   
-  end
-
-  def create
-    category = DailyCategory.new(daily_category_params)
-    category.update(user_id: 1)
-    if category.save
-      respond_with(data: category, status: 201)
-    else
-      respond_with(error: category.errors, status: 422)
-    end
-  end
-
-  def user_daily_categories
-    respond_with DailyCategory.where(user_id: params[:user_id])
-  end
-
-  private
-
-  def daily_category_params
-    params.require(:daily_category).permit(:kind)
-  end
+  
   
 end
---
+++ spec/controllers/api/v1/daily_categories_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Api::V1::DailyCategoriesController do 
+  describe 'GET #show' do
+    
+  end
+end
