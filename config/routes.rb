ec7490efe8de7e078dbcd1d1c40494fa51db175a
christian <christian.ambriz@gmail.com>
2016-11-06 11:18:32 -0800

    remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      # devise_for :users
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :daily_categories, only: [:show, :index, :create, :update, :destroy]
