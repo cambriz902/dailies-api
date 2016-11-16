23b46978855e361e5c90aa2863c84b9c2709123b
christian <christian.ambriz@gmail.com>
2016-11-15 16:54:45 -0800

    don't allow any website to use the api'

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module DailiesApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*','localhost:3000', 'http://localhost:3000'
+        origins 'localhost:3000', 'http://localhost:3000', 'http://localhost:4000'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
